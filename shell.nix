5cab3e1fefb0bb3a06ca2f4400afb89246037ecb
Luigi Foscari <luigi.foscari@icloud.com>
2022-02-12 15:51:09 +0100

    shell.nix changes

--- shell.nix
+++ shell.nix
@@ -1,23 +1,21 @@
-
 with import <nixpkgs> {};
 
 pkgs.mkShell {
 	buildInputs = with pkgs; [ maven jdk11 ];
 
-    JAVA_ARGS = "-Xmx2G";
+	JAVA_ARGS = "-Xmx2G";
 
-    BASENAME = "enwiki-2021";
-    BASEURL = "http://data.law.di.unimi.it/webdata";
+	BASENAME = "enwiki-2021";
+	BASEURL = "http://data.law.di.unimi.it/webdata";
 
 	shellHook = ''
-        cd graph
-        echo "Downloading graph and entities"
-        for ext in .properties .graph .md5sums .fcl; do
-            wget -qc $BASEURL/$BASENAME/$BASENAME$ext
-        done
+		set -e
 
-        echo "Computing offsets"
-        [ ! -f $BASENAME.offsets ] && java it.unimi.dsi.webgraph.BVGraph -o -O -L $BASENAME
-        cd ..
+		mkdir -p graph
+
+		echo "Downloading graph and entities"
+		for ext in properties graph md5sums fcl; do
+		    wget -c --output-document graph/$BASENAME.$ext $BASEURL/$BASENAME/$BASENAME.$ext
+		done
 	'';
 }
