95091d164f9674e6edde082a84e4f2d78398a55c
Luigi Foscari <luigi.foscari@icloud.com>
2022-03-20 10:43:55 +0100

    removed offset computation from nix shell

--- shell.nix
+++ shell.nix
@@ -16,20 +16,5 @@ pkgs.mkShell {
             echo -e '\t' graph/$BASENAME.$ext
 		    wget -qc --output-document graph/$BASENAME.$ext $BASEURL/$BASENAME/$BASENAME.$ext
 		done
-
-        cd wiki
-        echo "Setting classpath..."
-
-        mvn -q dependency:build-classpath -Dmdep.outputFile=classpath.txt
-        export CLASSPATH="`cat classpath.txt`:$CLASSPATH"
-
-        cd ..
-
-        if [ -f graph/$BASENAME.offsets ] && [ graph/$BASENAME.offsets -nt graph/$BASENAME.graph ]; then
-          echo "Offsets already present... skipping"
-        else
-          echo "Computing offsets..."
-          java it.unimi.dsi.webgraph.BVGraph -o -O -L graph/enwiki-2021
-        fi
 	'';
 }
